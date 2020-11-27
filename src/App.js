dee43a72488188c49ce407a61467103898439f5e
Alejandro Guespud <alejo.habbacuc@gmail.com>
2020-11-27 14:10:45 -0500

    base project ecommers coder house

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import emoji from './emoji.png'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Bienvenido a mi ecommerce apenas estamos por empezar </h1>
+     <br/>
+     <h1>Agradecemos tenga pasiencia. </h1>
+     <img src={emoji}></img>
+    </div>
+  );
+}
+
+export default App;
